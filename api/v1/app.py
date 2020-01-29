55143071c1a9668bb2ca78db3a4f017b7956b4a1
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-01-29 00:03:50 +0000

    error not found 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """api for hbnb project(airbnb clone)"""
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-from api.v1.views import app_views
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(x):
     """close storage after session"""
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", threaded=True)
-
