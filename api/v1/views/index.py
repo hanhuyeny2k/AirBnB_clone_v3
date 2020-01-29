61e59fd648dc2508f20216a563615dd7d3ce62df
trevor-ofarrell <744@holbertonschool.com>
2020-01-29 19:32:12 +0000

    removing jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,9 +14,7 @@ from models import storage
 
 @app_views.route('/status')
 def status():
-    return jsonify({
-        "status": "OK"
-    })
+    return {"status": "OK"}
 
 
 @app_views.route('/stats', methods=['GET'])
