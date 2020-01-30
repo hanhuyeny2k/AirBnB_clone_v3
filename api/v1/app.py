4f8e0b05ffbc8dc23dedaea1b52ab9343a64ba0d
trevor-ofarrell <744@holbertonschool.com>
2020-01-30 02:09:49 +0000

    fixing env vars

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,9 +18,8 @@ def teardown_db(x):
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """error handler for 404 not found"""
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    HBNB_API_HOST = '0.0.0.0'
-    HBNB_API_PORT = '5000'
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', '5000'), threaded=True)
